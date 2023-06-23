4d2f241a95a3873dd66079338bd92fd16d5ea889
AansRehman <106799284+AansRehman@users.noreply.github.com>
2023-06-24 00:19:48 +0500

    frontend react on the way

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import {Outlet} from "react-router-dom";
+
+import react from "react";
+
+const Layout = () => {
+    return(
+        <main>
+          <Outlet/>
+        </main>
+    )
+}
+export default Layout;
